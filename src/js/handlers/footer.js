58d438a3a04020eb02a7a969a1c07de11841ab7e
Felipe Freitag <felipefreitag08@hotmail.com>
2021-04-22 01:39:52 -0300

    Update footer and header events

--- src/js/handlers/footer.js
+++ src/js/handlers/footer.js
@@ -1,13 +1,13 @@
-import { displaySettingsMenu } from "./settings_menu";
+import { displayPerfilMenu } from "./perfil_menu";
 import { removeActive } from "./removeActive";
 import { displayHome } from "./main/home";
 
-const menuIcon = document.querySelector(".footer__button--bars");
+const perfilButton = document.querySelector(".footer__button--perfil");
 const homeButton = document.querySelector(".footer__button--home");
 const homeSection = document.querySelector(".home");
 
-menuIcon.addEventListener("click", event => {
-    displaySettingsMenu();
+perfilButton.addEventListener("click", event => {
+    displayPerfilMenu();
 });
 
 homeButton.addEventListener("click", event => {
--- src/js/handlers/header.js
+++ src/js/handlers/header.js
@@ -1,17 +1,11 @@
-import { displayPerfilMenu } from "./perfil_menu";
 import { removeActive } from "./removeActive";
 import { displayMyList } from "./main/myList";
 import { displaySeasonal } from "./main/seasonal";
 import { displayCategories } from "./main/categories";
 
-const perfilButton = document.querySelector(".header__button--perfil");
 const myListButton = document.querySelector(".header__button--myList");
 const seasonalButton = document.querySelector(".header__button--seasonal");
 const categoriesButton = document.querySelector(".header__button--categories");
-
-perfilButton.addEventListener("click", event => {
-    displayPerfilMenu();
-});
 
 myListButton.addEventListener("click", event => {
     removeActive();
