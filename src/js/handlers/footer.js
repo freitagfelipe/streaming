276759d7fe45ade7f292bcef665642298ab99f63
hugorplobo <hugorplobo@gmail.com>
2021-04-22 19:59:46 +0000

    Add search button event in footer.js

--- src/js/handlers/footer.js
+++ src/js/handlers/footer.js
@@ -1,10 +1,17 @@
 import { displayPerfilMenu } from "./perfil_menu";
 import { removeActive } from "./removeActive";
 import { displayHome } from "./main/home";
+import { displaySearchTab } from "./search";
 
 const perfilButton = document.querySelector(".footer__button--perfil");
 const homeButton = document.querySelector(".footer__button--home");
 const homeSection = document.querySelector(".home");
+
+const searchButton = document.querySelector(".footer__button--search");
+
+searchButton.addEventListener("click", event => {
+    displaySearchTab();
+});
 
 perfilButton.addEventListener("click", event => {
     displayPerfilMenu();
