d3bd4b10ecfddfb411e36c35feb5dba147b450c1
Felipe Freitag <felipefreitag08@hotmail.com>
2021-04-22 02:54:20 -0300

    Add events to profile buttons

--- src/js/handlers/perfil_menu.js
+++ src/js/handlers/perfil_menu.js
@@ -1,8 +1,26 @@
-import { displaySettingsMenu } from "./settings_menu";
+import { displayEditProfileMenu } from "./perfil_menu/edit_profile_menu";
+import { displayFriendsMenu } from "./perfil_menu/friends_menu";
+import { displayHistoryMenu } from "./perfil_menu/history_menu";
+import { displaySettingsMenu } from "./perfil_menu/settings_menu";
 
 const perfilMenu = document.querySelector(".perfil_menu");
 const closeMenu = document.querySelector(".perfil_menu__close_icon");
+const editProfileButton = document.querySelector(".perfil_menu__button--edit_profile");
+const friendsButton = document.querySelector(".perfil_menu__button--friends");
+const historyButton = document.querySelector(".perfil_menu__button--history");
 const settingsButton = document.querySelector(".perfil_menu__button--settings");
+
+editProfileButton.addEventListener("click", event => {
+    displayEditProfileMenu();
+});
+
+friendsButton.addEventListener("click", event => {
+    displayFriendsMenu();
+});
+
+historyButton.addEventListener("click", event => {
+    displayHistoryMenu();
+});
 
 settingsButton.addEventListener("click", event => {
     displaySettingsMenu();
